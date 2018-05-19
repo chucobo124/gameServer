93bcf685231c51f3c8f6b70b69dd6ed50a885a05
Lucas Chen <chucobo5219@gmail.com>
2018-05-19 14:45:17 +0800

    Add PUTRoom and GETRoom Endpoint

--- rooms/services.go
+++ rooms/services.go
@@ -1,6 +1,13 @@
 package rooms
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -49,10 +56,80 @@ func GetRooms(c *gin.Context) {
 
 // PutRoom User can join the room
 func PutRoom(c *gin.Context) {
-	// roomId := c.Param("room_id")
+	roomID, err := strconv.Atoi(c.Param("room_id"))
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+	} else if roomID == 0 {
+		returnErrorJSON(c, errors.New("The roomID is 0").Error())
+	}
+
+	putRoomPath := "https://myfistwebsite-204102.appspot.com/api/Room/" + fmt.Sprint(roomID)
+
+	type requestForm struct {
+		Name     string `json:"room_name" `
+		MaxUsers int    `json:"limit_users"`
+		Creator  string `json:"creator"`
+		IsActive bool   `json:"is_active"`
+	}
+	reqForm := new(requestForm)
+
+	type putRoomRequest struct {
+		ID       int    `json:"id"`
+		Name     string `json:"name"`
+		MaxUsers int    `json:"limitUsers"`
+		Creator  string `json:"creator"`
+		IsActive bool   `json:"isActive"`
+	}
+
+	err = c.BindJSON(reqForm)
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+	}
+
+	// Check validation
+	putRoomReq := putRoomRequest{}
+	if !reqForm.IsActive {
+		putRoomReq.IsActive = reqForm.IsActive
+	}
+
+	if reqForm.Creator != "" {
+		putRoomReq.Creator = reqForm.Creator
+	}
+
+	if reqForm.MaxUsers != 0 {
+		putRoomReq.MaxUsers = reqForm.MaxUsers
+	}
+
+	if reqForm.Name != "" {
+		putRoomReq.Name = reqForm.Name
+	}
+
+	jsonPutRoomReq, _ := json.Marshal(putRoomReq)
 	// Check room is exist?
+	req, err := http.NewRequest(http.MethodPost, putRoomPath, bytes.NewBuffer(jsonPutRoomReq))
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+		return
+	} else if resp.StatusCode != http.StatusOK {
+		returnErrorJSON(c, resp.Status)
+		return
+	}
 
 	c.JSON(200, gin.H{
-		"message": "I want to join the room" + c.Param("room_id"),
+		"message": "I want to join the room" + fmt.Sprint(roomID) + fmt.Sprint(
+			reqForm),
+	})
+}
+
+func returnErrorJSON(c *gin.Context, msg string) {
+	c.JSON(200, gin.H{
+		"error": msg,
 	})
 }
