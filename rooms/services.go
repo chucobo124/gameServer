185bb1ce48c966dcda4e4fac6facfe0366f31aa9
Lucas Chen <chucobo5219@gmail.com>
2018-05-19 17:01:18 +0800

    Get rooms

--- rooms/services.go
+++ rooms/services.go
@@ -5,52 +5,77 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"runtime/debug"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+// type getRoomsResp struct {
+// 	ID         int           `json:"id"`
+// 	Name       string        `json:"name"`
+// 	LimitUsers int           `json:"limitUsers"`
+// 	Creator    string        `json:"creator"`
+// 	IsActive   bool          `json:"isActive"`
+// 	CreDate    string        `json:"creDate"`
+// 	UpdDate    string        `json:"updDate"`
+// 	Game       []interface{} `json:"game"`
+// 	User       []user        `json:"user"`
+// }
+
+type GetRoomsResp struct {
+	ID         int           `json:"id"`
+	Name       string        `json:"name"`
+	LimitUsers int           `json:"limitUsers"`
+	Creator    string        `json:"creator"`
+	IsActive   bool          `json:"isActive"`
+	CreDate    string        `json:"creDate"`
+	UpdDate    string        `json:"updDate"`
+	Game       []interface{} `json:"game"`
+	User       []interface{} `json:"user"`
+}
+
+type User struct {
+	Name  string `json:"name"`
+	Coins int    `json:"coins"`
+}
+
+const externalRoomPath = "https://myfistwebsite-204102.appspot.com/api/Rooms/"
+const externalUserPath = "https://myfistwebsite-204102.appspot.com/api/Users/"
+
 // GetRooms Get Rooms Profile
 func GetRooms(c *gin.Context) {
+	userID := c.Query("user_id")
 	type room struct {
 		Name         string `json:"name"`
 		CurrentUsers int    `json:"current_user"`
 		MaxUser      int    `json:"max_user"`
 	}
-	type user struct {
-		Name  string `json:"name"`
-		Coins int    `json:"coins"`
+
+	rs, err := getRooms(c)
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+		return
 	}
-	r := []room{
-		room{
-			Name:         "RoomA",
-			CurrentUsers: 10,
-			MaxUser:      20,
-		},
-		room{
-			Name:         "RoomB",
-			CurrentUsers: 10,
-			MaxUser:      20,
-		},
-		room{
-			Name:         "RoomC",
-			CurrentUsers: 10,
-			MaxUser:      20,
-		},
-		room{
-			Name:         "RoomA",
-			CurrentUsers: 10,
-			MaxUser:      20,
-		},
+	// Convert Rooms Object to response
+	roomsCount := len(rs)
+	rooms := make([]room, roomsCount)
+	for i, r := range rs {
+		rooms[i].MaxUser = r.LimitUsers
+		rooms[i].Name = r.Name
 	}
-	u := user{
-		Name:  "Lucas",
-		Coins: 10,
+
+	user, err := getUserProfile(c, userID)
+	if err != nil {
+		returnErrorJSON(c, err.Error())
+		return
 	}
+
 	c.JSON(200, gin.H{
-		"rooms": r,
-		"user":  u,
+		"rooms": rooms,
+		"user":  user,
 	})
 }
 
@@ -59,11 +84,13 @@ func PutRoom(c *gin.Context) {
 	roomID, err := strconv.Atoi(c.Param("room_id"))
 	if err != nil {
 		returnErrorJSON(c, err.Error())
+		return
 	} else if roomID == 0 {
 		returnErrorJSON(c, errors.New("The roomID is 0").Error())
+		return
 	}
 
-	putRoomPath := "https://myfistwebsite-204102.appspot.com/api/Room/" + fmt.Sprint(roomID)
+	putRoomPath := externalRoomPath + fmt.Sprint(roomID)
 
 	type requestForm struct {
 		Name     string `json:"room_name" `
@@ -84,6 +111,7 @@ func PutRoom(c *gin.Context) {
 	err = c.BindJSON(reqForm)
 	if err != nil {
 		returnErrorJSON(c, err.Error())
+		return
 	}
 
 	// Check validation
@@ -129,7 +157,53 @@ func PutRoom(c *gin.Context) {
 }
 
 func returnErrorJSON(c *gin.Context, msg string) {
+	debug.PrintStack()
 	c.JSON(200, gin.H{
 		"error": msg,
 	})
+}
+
+func getRooms(c *gin.Context) ([]GetRoomsResp, error) {
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, externalRoomPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	getRoomResp := make([]GetRoomsResp, 0)
+	if err := json.Unmarshal(body, &getRoomResp); err != nil {
+		return nil, err
+	}
+	return getRoomResp, nil
+}
+
+func getUserProfile(c *gin.Context, userID string) (*User, error) {
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, externalUserPath+userID, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	currentUser := new(User)
+	if err := json.Unmarshal(body, &currentUser); err != nil {
+		return nil, err
+	}
+	return currentUser, nil
+
 }
